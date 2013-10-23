aebe07d3fead962bb6f7be7f596eaed237345cd7
Bruno Pop-Stefanov <brunopop@gatech.edu>
2013-10-22 20:36:36 -0400

    just changed the arguments of the main

--- Image/main.cpp
+++ Image/main.cpp
@@ -7,19 +7,19 @@ void Usage();
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc != 3)
 		Usage();
 
 	Extractor extractor;
-	//extractor.getTimeName(argv[argc-1]);
-	//extractor.video2images(argv[argc-1]);
-	extractor.readGeoData("C:\\Users\\Bruno\\Documents\\_SCHOOL\\GeorgiaTech\\2013 Fall\\CS 7495 Computer Vision\\Project 2\\20131019_133249\\Walkingfrom10thtoTSRB.txt");
+	extractor.getTimeName(argv[argc-2]);
+	extractor.video2images(argv[argc-2]);
+	extractor.readGeoData(argv[argc-1]);
 
 	return 0;
 }
 
 void Usage()
 {
-	std::cout << "Usage: CS7495P2.exe <video file>" << std::endl;
+	std::cout << "Usage: CS7495P2.exe <video file> <location file>" << std::endl;
 	exit(1);
 };

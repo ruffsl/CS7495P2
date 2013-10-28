e96b235e89b9670dae57befe14674ab761762536
brunopop <brunopop@hp.(none)>
2013-10-28 18:06:36 -0400

    cleaned Extractor a little

--- Image/Extractor.cpp
+++ Image/Extractor.cpp
@@ -15,28 +15,14 @@ namespace cs7495
 		delete firstFrameTime;
 	};
 
-	//void Extractor::breakVideo(const std::string& filepath)
-	//{
-	//	cv::VideoCapture cap;
-	//	if (cap.open(filepath))
-	//	{
-	//		while (true)
-	//		{
-	//			// Read next frame
-	//			Image tmp;
-	//			// If no next frame, then break
-	//			if (!cap.read(tmp)) break;
-	//			// Otherwise, add it to the vector of features
-	//			frames.push_back(tmp);
-	//		}
-	//		cap.release();
-	//	}
-	//};
-
-	void Extractor::video2images(const std::string& filepath)
+	void Extractor::video2images(const string& filepath)
 	{
-		cout << "Extractor::video2images(const std::string& filepath)" << endl;
-		// Time of frame
+		if (timestamps.empty())
+		{
+			cerr << "Error: No GPS locations and time stamps on record! Aborting reading video..." << endl;
+			return;
+		}
+		// Time of first frame
 		local_date_time frameTime = *firstFrameTime;
 		// Counter on the number of frames
 		int counter = 0;
@@ -45,6 +31,7 @@ namespace cs7495
 		// If successful
 		if (cap.isOpened())
 		{
+			cout << "Reading every frame of " << filepath << endl;
 			// For all frames
 			while (true)
 			{
@@ -53,12 +40,10 @@ namespace cs7495
 				// If no next frame, then break
 				if (!cap.read(tmp)) break;
 
-				// 2. Find corresponding time stamp and thus GPS coordinates
+				// 2. Find corresponding time stamp and, therefore, GPS coordinates
 				// Initialize to first
-				cout << "size of timestamps: " << (timestamps.empty() ? 0 : timestamps.size()) << endl;
 				boost::posix_time::time_duration duration = *firstFrameTime - timestamps[0];
-				cout << "long min = std::abs( duration.total_milliseconds() );" << endl;
-				long min = std::abs( duration.total_milliseconds() );
+				long min = abs( duration.total_milliseconds() );
 				unsigned int index = 0;
 				// Iterate through all saved coordinates/time stamps
 				for (auto t : timestamps)
@@ -95,22 +80,15 @@ namespace cs7495
 		}
 		else
 		{
-			std::cout << "Error: could not open " << filepath << std::endl;
+			cerr << "Error: could not open " << filepath << endl;
 		}
 	};
 
-
-	void Extractor::getTimeName(const std::string& filepath)
+	void Extractor::getTimeName(const string& filepath)
 	{
+		cout << "Parsing time of first frame." << endl;
 		// Get file name
-		char slash;
-#ifdef _WIN32
-		slash = '\\';
-#else
-		slash = '\/';
-#endif
-		std::vector<std::string> vect1 = split(filepath, slash);
-
+		std::vector<std::string> vect1 = split(filepath, '/');
 		// Remove extension
 		std::vector<std::string> vect2 = split(vect1[vect1.size()-1], '.');
 		// Split
@@ -125,38 +103,39 @@ namespace cs7495
 		ss.imbue(std::locale(ss.getloc(), input_facet));
 		ss.str(date);
 		ss >> *firstFrameTime;
-
-		std::cout << "Full Time:\t"   << firstFrameTime->to_string() << std::endl;
-		std::cout << "Local time:\t"  << firstFrameTime->local_time() << std::endl;
-		std::cout << "Time zone:\t"   << firstFrameTime->zone_as_posix_string() << std::endl;
-		std::cout << "Zone abbrev:\t" << firstFrameTime->zone_abbrev() << std::endl;
-		std::cout << "Zone offset:\t" << firstFrameTime->zone_abbrev(true) << std::endl;
+#ifdef DEBUG
+		cout << "Time of the first frame:" << endl;
+		cout << "\tLocal time:\t"  << firstFrameTime->local_time() << endl;
+		cout << "\tTime zone:\t"   << firstFrameTime->zone_as_posix_string() << endl;
+		cout << "\tZone abbrev:\t" << firstFrameTime->zone_abbrev() << endl;
+		cout << "\tZone offset:\t" << firstFrameTime->zone_abbrev(true) << endl;
+#endif
 	};
 
-	void Extractor::readGeoData(const std::string& filepath)
+	void Extractor::readGeoData(const string& filepath)
 	{
 		// Open text file
-		std::ifstream myfile(filepath.c_str());
+		ifstream myfile(filepath.c_str());
 		if (!myfile.is_open())
 		{
 			cerr << "Could not open " << filepath << endl;
 			return;
 		}
-
-		std::string line;
+		cout << "Parsing " << filepath << " for time stamps and GPS locations." << endl;
+		string line;
 		// Parse line by line
-		while ( std::getline(myfile, line) )
+		while ( getline(myfile, line) )
 		{
 			// Skip empty lines
 			if ( line.compare("") == 0)
 				continue;
 			// Tokenize line
-			std::vector<std::string> tokens = split(line, ' ');
+			vector<string> tokens = split(line, ' ');
 			// If line starts with 'T'
 			if (tokens[0].compare("T") == 0)
 			{
-				// Add time stamp
-				std::string timestamp = "";
+				// 1. Add time stamp
+				string timestamp = "";
 				timestamp += tokens[1] + " " + tokens[2] + " UTC+4";
 				// Set up the input datetime format.
 				local_time_input_facet *input_facet = new local_time_input_facet("%Y-%m-%d %H:%M:%S%f %ZP");
@@ -169,89 +148,12 @@ namespace cs7495
 				// Insert boost local_time into list
 				timestamps.push_back(ldt);
 
-				//std::cout << "\nFull Time:\t"   << ldt.to_string() << std::endl;
-				//std::cout << "Local time:\t"  << ldt.local_time() << std::endl;
-				//std::cout << "Time zone:\t"   << ldt.zone_as_posix_string() << std::endl;
-				//std::cout << "Zone abbrev:\t" << ldt.zone_abbrev() << std::endl;
-				//std::cout << "Zone offset:\t" << ldt.zone_abbrev(true) << std::endl;
-
-				//boost::posix_time::time_duration diff = *firstFrameTime - ldt;
-				//std::cout << "Difference (h):\t" << diff.hours() << std::endl;
-				//std::cout << "Difference (m):\t" << diff.minutes() << std::endl;
-				//std::cout << "Difference (s):\t" << diff.total_seconds() << std::endl;
-				//std::cout << "Difference (ms):" << diff.total_milliseconds() << std::endl;
-
-				// Add GPS coordinates
+				// 2. Add GPS coordinates
 				vector<double> coord;
 				coord.push_back(atof(tokens[3].c_str()));
 				coord.push_back(atof(tokens[4].c_str()));
 				GPScoord.push_back(coord);
 			}
-		}
-	};
-
-	unsigned int Extractor::findFirstTimeStamp()
-	{
-		// Initialize to first
-		long min = std::abs( (*firstFrameTime - timestamps[0]).total_milliseconds() );
-		unsigned int index = 0;
-		// Iterate through all coordinates/time stamps
-		for (auto t : timestamps)
-		{
-			// Compute time difference
-			boost::posix_time::time_duration diff = *firstFrameTime - t;
-			// Memorize the smallest difference and the pair corresponding to it
-			if ( std::abs(diff.total_milliseconds()) < min)
-			{
-				min = diff.total_milliseconds();
-				index++;
-			}
-			// If the difference becomes <0 then we passed
-			// the pair we were looking for. stop
-			if ( diff.total_milliseconds() < 0 )
-				break;
-		}
-		// Return the index corresponding to the first frame
-#ifdef _DEBUG
-		std::cout << "The first frame has time " << firstFrameTime->to_string() << std::endl;
-		std::cout << "The corresponding <GPS,time> pair is:\n\ttime: " << timestamps[index].to_string()
-			<< "\n\t gps: " << GPScoord[index][0] << ", " << GPScoord[index][1] << std::endl;
-#endif
-		return index;
-	};
-
-	void Extractor::findCorrespondingGPSCoords()
-	{
-		// Time of frame
-		local_date_time frameTime = *firstFrameTime;
-		// For all frames
-		int n = 0;
-		while(n < 100) // stop when no more frames
-		{
-			// Initialize to first
-			long min = std::abs( (*firstFrameTime - timestamps[0]).total_milliseconds() );
-			unsigned int index = 0;
-			// Iterate through all coordinates/time stamps
-			for (auto t : timestamps)
-			{
-				// Compute time difference
-				boost::posix_time::time_duration diff = frameTime - t;
-				// Memorize the smallest difference and the pair corresponding to it
-				if ( std::abs(diff.total_milliseconds()) < min)
-				{
-					min = diff.total_milliseconds();
-					index++;
-				}
-				// If the difference becomes <0 then we passed
-				// the pair we were looking for. stop
-				if ( diff.total_milliseconds() < 0 )
-					break;
-			}
-			// Return the index corresponding to the first frame
-			//correspondingGPS.push_back(GPScoord[index]);
-			// Increment time of frame
-			frameTime += boost::posix_time::seconds(1);
-			n++;
 		}
 	};
 }
--- Image/Extractor.h
+++ Image/Extractor.h
@@ -1,6 +1,6 @@
 #include "Image.h"
 #include "utils.h"
-//#define BOOST_DATE_TIME_NO_LIB
+#define BOOST_DATE_TIME_NO_LIB
 #include <boost/date_time/local_time/local_time.hpp>
 
 using namespace boost::local_time;
@@ -14,13 +14,10 @@ namespace cs7495
 	/// </summary>
 	class Extractor
 	{
-	public:
-	  vector<Image> frames;				///< Contains all the frames in the video
-	  local_date_time *firstFrameTime;	///< Reference time stamp (when recording started)
+	private:
+	  local_date_time *firstFrameTime;		///< Reference time stamp (when recording started)
 	  vector<local_date_time> timestamps;	///< List of time stamps written as strings, e.g. "2013-10-19 17:32:43.537"
-	  vector<vector<double> > GPScoord;	///< List of GPS coordinates written as an array of two floating point numbers, e.g. {33.782714000, -84.395934000}
-
-	public: unsigned int findFirstTimeStamp();
+	  vector<vector<double> > GPScoord;		///< List of GPS coordinates written as an array of two floating point numbers, e.g. {33.782714000, -84.395934000}
 
 	public:
 		/// <summary>
@@ -31,32 +28,25 @@ namespace cs7495
 		~Extractor();
 
 		/// <summary>
-		/// Extract all frames from a video file.
+		/// Extract each frame, find the correspond GPS location
+		/// from its time stamp and save SIFT key points to a
+		/// formatted text file.
 		/// </summary>
 		/// <param name="filepath">[in] Path to the video file</param>
-		//void breakVideo(const std::string& filepath);
-
-		/// <summary>
-		/// Extract each frame and saves it as a JPEG file
-		/// without keeping it in memory.
-		/// </summary>
-		/// <param name="filepath">[in] Path to the video file</param>
-		void video2images(const std::string& filepath);
+		void video2images(const string& filepath);
 
 
 		/// <summary>
-		/// Extract time from and MP4 file name
+		/// Parse time from and MP4 file name
 		/// </summary>
 		/// <param name="filepath">[in] Path to the video file</param>
-		void getTimeName(const std::string& filepath);
+		void getTimeName(const string& filepath);
 
 		/// <summary>
 		/// Open a text file containing time stamps and GPS coordinates
 		/// and read them in.
 		/// </summary>
 		/// <param name=""filepath">[in] Path to the text file</param>
-		void readGeoData(const std::string& filepath);
-
-		void findCorrespondingGPSCoords();
+		void readGeoData(const string& filepath);
 	};
 }
